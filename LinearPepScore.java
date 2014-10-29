3dfa68b4767201b3f3cfccefdea56e24506c2e86
breezedu <breeze.du@gmail.com>
2014-10-29 16:08:46 -0400

    update LinearPepScore, add compareTo() method

--- LinearPepScore.java
+++ LinearPepScore.java
@@ -12,9 +12,23 @@ public class LinearPepScore implements Comparable<LinearPepScore>{
 		
 	}
 	
+	//is this package, we will sort LinearPepScore in descend order;
+	//so the compare will return -1 if this.score > that.score;
 	public int compareTo(LinearPepScore linearPepScore) {  
-	       return (this.score >= linearPepScore.score ) ? -1: (this.score < linearPepScore.score ) ? 1:0 ;  
-	    }  
+	    
+		return (this.getScore() > linearPepScore.getScore() ) ? -1:0 ;  
+	}  
+	
+	
+	public int getScore() {
+	
+		return this.score;
+	}
+	
+	
+	public String getSeq() {
 		
+		return this.seq;
+	}
 	
 }//end class LinearpepScore class;
