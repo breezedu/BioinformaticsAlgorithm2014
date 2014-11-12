f4e9881733019f0a5d70f57f88d688f7577b5980
breezedu <breeze.du@gmail.com>
2014-11-11 23:44:14 -0500

    add CBB520HW2_Part3.java scripts

---
+++ CBB520HW2_Part3_ExtractUnMapped.java
@@ -0,0 +1,85 @@
+package courseraBioinformatics2014;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CBB520HW2_Part3_ExtractUnMapped {
+	
+	public static void main(String[] args) throws IOException{
+		
+		System.out.println("Extract unmapped sequence from ver6h_short.sam document.");
+		
+		
+		//1st, create a scanner to readin data
+		//data document locates: D:\BioinformaticsCoursera\TXT\cbb520
+		Scanner readIn = new Scanner(new File("D:/BioinformaticsCoursera/TXT/cbb520/ver6h.sam"));
+		
+		
+		//2nd, create a writter to write unmapped sequence into a fastq file
+		String routine = "D:/BioinformaticsCoursera/TXT/cbb520/";
+		String doc_name = "ver6h_unmapped.fastq";
+		
+		File output_file = new File(routine + doc_name);
+		BufferedWriter output = new BufferedWriter(new FileWriter(output_file));	
+		
+		
+		//the first 2000 lines are short information like: 
+		/*********************
+		@SQ     SN:45   LN:87
+		@SQ     SN:46   LN:87
+		@SQ     SN:50   LN:67
+		@SQ     SN:53   LN:67
+		@SQ     SN:54   LN:65
+		@SQ     SN:55   LN:87
+		*/
+		
+		//the line with * in the third colum means that read could not be aligned to the reference:
+		/*********************
+		 * D5ZTB5M1:245:D0GT6ACXX:8:1101:5240:3449 77      *       0       0       *       *       0       0       
+		 *   AGCGACAGAGAGGGCAAAAGAAAATAAAAGTAAGATTTTAGTTTGTAATGGGAGGGGGGGTTTAGTCATGGAGTACAAGTGTGAGGAAAAGTAGTTGGGAG   
+		 *   :;8=?:<<:<:+A@:C8EGA=?G;E9CCDG?F>>BF<FE4;DDAFFF>FE>F3;A';857B7?A<443@AA@?:?A@A>>3@A:4<>?A@B4:A@ABA9@@
+		 
+		 * D5ZTB5M1:245:D0GT6ACXX:8:1101:5240:3449 141     *       0       0       *       *       0       0       
+		 *   TTTCTTCCCAAATTGTATCTCTCAATACGCATCAACCCATGTCAATTAAACACGCTGTATAGAGACTAGGCAGATCTGACGATCACCTAGCGACTCTCTCC   
+		 *   8?@BDA>DABFDH@+<,ACAAI3A@?EBBBEHGBGG88?*)*:BBBGFHI<?F<EEG@GCCFF;@GGEEE>CA??@D>(.9;?AB>=AC@C=?<95A34:3
+
+		 */
+		int count = 0;
+		int unmapped = 0;
+		while(readIn.hasNextLine()){
+			
+			String currLine = readIn.nextLine();
+			
+			
+			if(currLine.length() > 100){
+				
+				count++;
+				
+				String[] str = currLine.split("\t");
+				
+				if(str[2].equals("*")){
+					
+					unmapped++;
+					
+					output.write("@" + str[0] +"\n");
+					output.write(str[9] + "\t" + str[10] + "\n\n");
+					
+				}
+			}
+			
+		}
+		
+		System.out.println("Of all the " +count + " reads, " + unmapped + " could not be mapped into the reference.");
+		double ratio = (double) unmapped/count;
+		System.out.println("The ratio is: " + ratio*100 + "%.");
+
+		//close scanner and writter;
+		readIn.close();
+		output.close();
+		
+	}//end main();
+
+}
